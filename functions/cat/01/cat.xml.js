e9b470522e136e258d5c54259d3afa19ed1d0cf9
nengmaya538 <nengmaya538@gmail.com>
2026-03-16 21:45:51 +0700

    Update cat.xml.js

--- functions/cat/01/cat.xml.js
+++ functions/cat/01/cat.xml.js
@@ -1,1 +1,28 @@
+export async function onRequestGet(context) {
+  const host = context.request.headers.get("host");
 
+  const urls = [
+    "/",
+    "/artikel01/",
+    "/artikel02/",
+    "/artikel03/"
+  ];
+
+  const items = urls.map(url => {
+    return `
+<url>
+  <loc>https://${host}${url}</loc>
+</url>`;
+  }).join("");
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${items}
+</urlset>`;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "application/xml"
+    }
+  });
+}
